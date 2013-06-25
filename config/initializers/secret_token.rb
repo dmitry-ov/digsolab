0809299e06fd33ab6be65ad99bd7e2424215c016
Dmitry Ovcharenko <ovcharenkodmitry@gmail.com>
2013-06-25 12:38:18 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Digsolab::Application.config.secret_token = 'ca018a1d428f36ce62dcca359a6524899a155c7f6d717a65cfde1df98b601de163c2b553cc5e27c63904db58efd0ed9afa97e69b482002be607ebd21b4c3b622'
